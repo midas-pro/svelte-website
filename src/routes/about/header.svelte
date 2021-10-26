8eaad3d20d469842c02d376f246269d2c1702e38
midas-tiger <roberttimu091@gmail.com>
2021-10-26 20:09:53 +0700

    Update header.svelte

--- src/routes/about/header.svelte
+++ src/routes/about/header.svelte
@@ -1,16 +1,28 @@
 <div class="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
     <div class="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
-      <img alt="sapper svelte themes template pembuatan website" src="https://1.bp.blogspot.com/-WhpGrb3WTdM/YLefLJBoylI/AAAAAAAAOkE/By8IuHDEQUANd1m7EO0T5BrEQGNtiJAzwCLcBGAsYHQ/s2048/11942070_10204831737534195_4822690879798825028_o.jpg" alt="Two each of gray, white, and black shirts laying flat." class="w-full h-full object-center object-cover">
+      <img 
+           alt="sapper svelte themes template pembuatan website" 
+           src="https://1.bp.blogspot.com/-WhpGrb3WTdM/YLefLJBoylI/AAAAAAAAOkE/By8IuHDEQUANd1m7EO0T5BrEQGNtiJAzwCLcBGAsYHQ/s2048/11942070_10204831737534195_4822690879798825028_o.jpg" 
+           class="w-full h-full object-center object-cover">
     </div>
     <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
       <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-        <img alt="sapper svelte themes template pembuatan website" src="https://1.bp.blogspot.com/-ccrjaoINTw0/YLeWe5rkoiI/AAAAAAAAOf4/fxRLUrRjRmchszp585-F6NlcVh7i7ohaACLcBGAsYHQ/s1500/11174240_10204154961375214_5303596129448376184_o.jpg" alt="Model wearing plain black basic tee." class="w-full h-full object-center object-cover">
+        <img 
+             alt="sapper svelte themes template pembuatan website" 
+             src="https://1.bp.blogspot.com/-ccrjaoINTw0/YLeWe5rkoiI/AAAAAAAAOf4/fxRLUrRjRmchszp585-F6NlcVh7i7ohaACLcBGAsYHQ/s1500/11174240_10204154961375214_5303596129448376184_o.jpg" 
+             class="w-full h-full object-center object-cover">
       </div>
       <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-        <img alt="sapper svelte themes template pembuatan website" src="https://1.bp.blogspot.com/-0uWlh8aWUWY/YLeWmtKlmCI/AAAAAAAAOgs/S4ioMiCX-TAQ8XuHnoumXSD6wv5HfuMmwCLcBGAsYHQ/s2048/13422394_10206632630875403_1612614148694737501_o.jpg" alt="Model wearing plain gray basic tee." class="w-full h-full object-center object-cover">
+        <img 
+             alt="sapper svelte themes template pembuatan website" 
+             src="https://1.bp.blogspot.com/-0uWlh8aWUWY/YLeWmtKlmCI/AAAAAAAAOgs/S4ioMiCX-TAQ8XuHnoumXSD6wv5HfuMmwCLcBGAsYHQ/s2048/13422394_10206632630875403_1612614148694737501_o.jpg"
+             class="w-full h-full object-center object-cover">
       </div>
     </div>
     <div class="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-      <img alt="sapper svelte themes template pembuatan website" src="https://1.bp.blogspot.com/-c6m0yBGiKsI/YLeWs2KeBuI/AAAAAAAAOhg/0S9UcKNOvy8o1aieBR_ObZi2FI8gEKKAwCLcBGAsYHQ/s2048/16700509_10208565805163552_1504683341332760374_o.jpg" alt="Model wearing plain white basic tee." class="w-full h-full object-center object-cover">
+      <img 
+           alt="sapper svelte themes template pembuatan website" 
+           src="https://1.bp.blogspot.com/-c6m0yBGiKsI/YLeWs2KeBuI/AAAAAAAAOhg/0S9UcKNOvy8o1aieBR_ObZi2FI8gEKKAwCLcBGAsYHQ/s2048/16700509_10208565805163552_1504683341332760374_o.jpg" 
+           class="w-full h-full object-center object-cover">
     </div>
   </div>
